731541a08ef9d05d4dcf34c838839cd7e4449070
JiaweiZhuang <jiaweizhuang@g.harvard.edu>
2018-01-30 18:41:16 -0500

    typo

--- build_library/NetCDF_test/test_nc.sh
+++ build_library/NetCDF_test/test_nc.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # -L/usr/lib can be omiited
-gfortran simple_xr_wr.f90 -o write_nc_f90.exe -I/usr/include -lnetcdff
+gfortran simple_xy_wr.f90 -o write_nc_f90.exe -I/usr/include -lnetcdff
 ./write_nc_f90.exe
 ncdump -h simple_xy_f90.nc
 
-gfortran simple_xr_wr.f -o write_nc_f77.exe -I/usr/include -lnetcdff
+gfortran simple_xy_wr.f -o write_nc_f77.exe -I/usr/include -lnetcdff
 ./write_nc_f77.exe
 ncdump -h simple_xy_f77.nc

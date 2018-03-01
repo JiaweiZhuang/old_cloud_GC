86ec2167905b2e70a0350430e1afa2dfd5a1a6c5
JiaweiZhuang <jiaweizhuang@g.harvard.edu>
2018-03-01 14:44:02 -0500

    Simplify ubuntu build script to a single command

--- build_library/quick_build_ubuntu.sh
+++ build_library/quick_build_ubuntu.sh
@@ -4,7 +4,7 @@
 # Tested on EC2 ubuntu-xenial-16.04 (ami-80861296)
 
 sudo apt-get update
-sudo apt-get install -y bc # calculator. GC calls it at compile time
-sudo apt-get install -y gcc gfortran # compiler
-sudo apt-get install -y libnetcdf-dev libnetcdff-dev # NC lib
-sudo apt-get install -y netcdf-bin ncview # NC analysis tool
+sudo apt-get install -y \
+    bc gcc gfortran \
+    libnetcdf-dev libnetcdff-dev \
+    netcdf-bin ncview
